eb993716d8e636ce22629db9de1ce42d983dcc99
Abeni <abenezeryirgalem0@gmail.com>
2025-05-03 00:23:48 +0300

    Added Routing for all the pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -8,27 +11,60 @@ import ListingDetailPage from "./pages/ListingDetails";
 import MarketplacePage from "./pages/MarketplacePage";
 import Account from "./pages/Accout";
 import About from "./pages/About";
-import Footer from "./components/Footer";
 import ContactUs from "./pages/ContactUs";
 import CartPage from "./pages/CartPage";
+import Dashboard from "./pages/Dashboard";
+import SimilarItemsPage from "./pages/SimilarItemsPage";
 
 function App() {
   return (
     <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/create" element={<CreateListingPage />} />
-          <Route path="/listings" element={<MarketplacePage />} />
-          <Route path="/listing/:id" element={<ListingDetailPage />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-        <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/listings" element={<MarketplacePage />} />
+        <Route path="/listing/:id" element={<ListingDetailPage />} />
+        <Route path="/similar/:id" element={<SimilarItemsPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/create"
+          element={
+            <PrivateRoute>
+              <CreateListingPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <PrivateRoute>
+              <Account />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <CartPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
